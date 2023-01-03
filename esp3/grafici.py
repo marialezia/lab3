f5013801c58f21c6864c6fb13ef54e5485262ac9
marialezia <mlbartoccini@gmail.com>
2023-01-03 20:18:06 +0100

    corretto codice pendolo

--- esp3/grafici.py
+++ esp3/grafici.py
@@ -23,9 +23,16 @@ def periodo(volt, incremento, oscillazioni):
             break
         prima = dopo
     periodo = (stop-start)*incremento*2/oscillazioni
-    return periodo
+    periodoErr = incremento*2/oscillazioni
+    return periodo, periodoErr
 
-l = 0.38
+def gravitaErr(T,TErr, l, lErr):
+    return 4*np.pi**2/T**2*np.sqrt(lErr**2+TErr**2/T**2)
+
+lunghezza = 0.38
+lunghezzaErr = 0.0001/(12)**0.5
+
+print(lunghezzaErr)
 
 #prendo dati da csv, SCALA 1S
 scala_1s = pd.read_csv('scala_1s.csv')
@@ -79,12 +86,14 @@ plt.xlabel('Tempo (s)')
 plt.ylabel('Volt (v)')
 plt.show()
 
+
 plt.plot(tempi2s_1, v2s_1, '-o', markersize = 3, color = 'rosybrown')
 plt.title('Scala 2s - prima prova')
 plt.xlabel('Tempo (s)')
 plt.ylabel('Volt (v)')
 plt.grid()
 plt.show()
+
 
 plt.plot(tempi2s_2, v2s_2,  '-o', markersize = 3, color= 'salmon')
 plt.title('Scala 2s - seconda prova')
@@ -114,47 +123,4 @@ plt.ylabel('Volt (v)')
 plt.grid()
 plt.show()
 
-#calcolo tempo totale dieci oscillazioni con ciclo for che guarda quando il valore del volt si abbassa sotto 6 per vedere dove ci sono i picchi 
 
-#set 1s
-
-periodo1s = periodo(v1s, t1s, 10)
-g1s = gravita(periodo1s, l)
-
-
-#set 2s prima prova
-
-periodo2s_1 = periodo(v2s_1, t2s_1, 10)
-g2s_1 = gravita(periodo2s_1, l)
-
-
-#set 2s seconda prova
-
-periodo2s_2 = periodo(v2s_2, t2s_2, 10)
-g2s_2 = gravita(periodo2s_2, l)
-
-#set 2s terza prova
-
-periodo2s_3 = periodo(v2s_3, t2s_3, 10)
-g2s_3 = gravita(periodo2s_3, l)
-
-
-#set x prima prova
-
-periodoxs_1 = periodo(vxs_1, txs_1, 6 )
-gxs_1 = gravita(periodoxs_1, l)
-
-
-#set x seconda prova
-
-periodoxs_2 = periodo(vxs_2, txs_2, 10)
-gxs_2 = gravita(periodoxs_2, l)
-
-tabella = pd.DataFrame()
-tabella['Scala'] = ('1s', '2s_1', '2s_2', '2s_3', 'xs_1', 'xs_2')
-tabella['T'] = (periodo1s, periodo2s_1, periodo2s_2, periodo2s_3, periodoxs_1, periodoxs_2)
-tabella['g'] = (g1s, g2s_1, g2s_2, g2s_3, gxs_1, gxs_2)
-
-print(tabella)
-
-print(txs_1, txs_2)
