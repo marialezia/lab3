7f5bac159e247c1cd3ea8f1b3c9dc9a3ad188eb1
marialezia <mlbartoccini@gmail.com>
2023-01-07 18:50:41 +0100

    aggiornato diapason

--- esp5/pulitaPezzo.py
+++ esp5/pulitaPezzo.py
@@ -7,7 +7,7 @@ import funzioni as fz
 
 
 #apro il file .wav
-data, samplerate = sf.read('diapason.wav')
+data, samplerate = sf.read('pulita_pezzo.wav')
 
 print('samplerate = ', samplerate)
 print('data = ', data)
@@ -22,13 +22,13 @@ t = 1/samplerate
 durata = len(y)/samplerate
 x = np.arange(0, durata, t)
 
-'''
+
 plt.title('Waveform diapason')
 plt.plot(x, y, color = 'seagreen')
 plt.xlabel('Tempo[s]')
 plt.ylabel('Ampiezza (u.a)')
 plt.show()
-'''
+
 #creo un nuovo file .wav uguale al primo
 #sf.write('diapason_recreated.wav', data, samplerate)
 
@@ -46,7 +46,7 @@ print('frequenze tf = ', fftfreq)
 print('lunghezza frequenze = ', len(fftfreq))
 
 #grafico ampiezze reale e immaginaria
-'''
+
 fig, ax = plt.subplots(2,1, figsize=(40,40))
 ax[0].plot(fftfreq, datafft.real, color='rebeccapurple')
 ax[1].plot(fftfreq, datafft.imag, color='cyan')
@@ -58,48 +58,14 @@ ax[1].set_xlabel('Frequenza (Hz)')
 ax[1].set_ylabel('Ampiezza immaginaria (u.a.)')
 fig.suptitle('fft')
 plt.show()
-'''
-#grafico potenza con massimi (VECCHIA VERSIONE MASSIMI)
 
 
-'''
-max1 = np.argmax(p)
-max2 = np.argmax(p[400:len(p)])
-max3 = np.argmax(p[1800:len(p)])
-
-
-plt.plot(fftfreq, p,'-o', markersize = 1, color='limegreen')
-plt.plot(fftfreq[max1], p[max1], 'o', color='forestgreen')
-plt.plot(fftfreq[400:len(p)][max2], p[400:len(p)][max2], 'o', color='forestgreen')
-plt.plot(fftfreq[1800:len(p)][max3], p[1800:len(p)][max3], 'o', color='forestgreen')
-plt.xlabel('Frequenza (hz)')
-plt.ylabel('Potenza (u.a)')
-plt.show()
-
-
-max4 = np.argmax(datafft)
-max5 = np.argmax(datafft[700:1800])
-max6 = np.argmax(datafft[1800:len(datafft)])
-min1 = np.argmin(datafft)
-min2 = np.argmin(datafft[700:len(datafft)])
-min3 = np.argmin(datafft[1800:len(datafft)])
-
-#grafico parte reale con massimi e minimi
-plt.plot(fftfreq, datafft.real, color='lightsalmon')
-plt.plot(fftfreq[max4], datafft[max4].real, 'o', color='tomato')
-plt.plot(fftfreq[700:1800][max5], datafft[700:1800][max5].real, 'o', color='tomato')
-plt.plot(fftfreq[1800:len(datafft)][max6], datafft[1800:len(datafft)][max6].real, 'o', color='tomato')
-plt.plot(fftfreq[min1], datafft[min1].real, 'o', color='tomato')
-plt.plot(fftfreq[700:len(datafft)][min2], datafft[700:len(datafft)][min2].real, 'o', color='tomato')
-plt.plot(fftfreq[1800:len(datafft)][min3], datafft[1800:len(datafft)][min3].real, 'o', color='tomato')
-plt.show()
-'''
 
 
 
 #trovo la posizione dei massimi con funzione
 sogliaa = 200000
-maxx = fz.massimi(p, sogliaa,3)
+maxx = fz.massimi(p, sogliaa,68)
 
 
 plt.plot(fftfreq, p,'-o', markersize = 1, color='limegreen')
@@ -230,12 +196,12 @@ dataSint3 = fft.irfft(filtropp3, n = len(y))
 dataSint4 = fft.irfft(filtropp4, n = len(y))
 
 #produco file audio
-                       
+'''                      
 sf.write('diapasonDataSint1.wav', dataSint1, samplerate)
 sf.write('diapasonDataSint2.wav', dataSint2, samplerate)
 sf.write('diapasonDataSint3.wav', dataSint3, samplerate)
 sf.write('diapasonDataSint4.wav', dataSint4, samplerate)
-
+'''
 #grafico waveform
 fig,ax = plt.subplots(2,2, figsize=(12,12) )
 ax[0][0].set_title('picco principale')
